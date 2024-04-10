b82fdb0d01b4e6d94016cc21bc3fd6c603a9f3be
Matthew <matthewcollinsece@gmail.com>
2024-04-10 13:09:59 -0400

    Updated parser to save CVE data to cve_icsa_join and cve_list tabels

--- rss/rss_parser.py
+++ rss/rss_parser.py
@@ -14,7 +14,7 @@ RSS_URL = 'https://www.cisa.gov/cybersecurity-advisories/ics-advisories.xml'
 DB_PARAMS = {
     'dbname': 'chariots',
     'user': 'postgres',
-    'password': '<insert_password>',
+    'password': 'DrBanikIsAwesome',
     'host': 'ics_db',
     #'host': '24.192.91.200',
     'port': 5432
@@ -43,6 +43,8 @@ def init_db():
 def fetch_and_update():
     feed = feedparser.parse(RSS_URL)
     entries = []
+    cve_pattern = re.compile(r"CVE-\d{4}-\d{4,7}")  # Regular expression pattern for CVE identifiers
+    cve_to_icsas = {}
 
     for entry in feed.entries:
         original_date = datetime.strptime(entry.published, '%a, %d %b %y %H:%M:%S %z')
@@ -54,7 +56,19 @@ def fetch_and_update():
 
         # Parse the summary HTML to find the vendor name
         soup = BeautifulSoup(entry.summary, 'html.parser')
+
+        # Set vendor to default of unkown
         vendor = "Unknown"
+
+        #Find all CVEs in HTML
+        cves_found = set(cve_pattern.findall(entry.summary) + cve_pattern.findall(title))  # Search in summary and title
+        
+        # Map each found CVE to its corresponding ICSA advisory number
+        for cve in cves_found:
+            if cve not in cve_to_icsas:
+                cve_to_icsas[cve] = []
+            cve_to_icsas[cve].append(advisory_number)
+
         for li in soup.find_all('li'):
             if 'Vendor' in li.text:
                 vendor_text = li.get_text()
@@ -72,12 +86,25 @@ def fetch_and_update():
     
     with closing(psycopg2.connect(**DB_PARAMS)) as conn:
         with conn.cursor() as cur:
+            # Assuming 'entries' is prepared beforehand
             execute_values(cur,
                            '''
                            INSERT INTO advisory_list (date, title, icsa, link, html_dir, vendor)
                            VALUES %s ON CONFLICT (link) DO NOTHING
                            ''',
                            entries)
+
+            for cve, icsas in cve_to_icsas.items():
+                # Insert the CVE into the cve_list table, ignore duplicates
+                cur.execute("INSERT INTO cve_list (cve_id) VALUES (%s) ON CONFLICT (cve_id) DO NOTHING", (cve,))
+
+                unique_icsas = set(icsas)  # Remove duplicates of ICSAs for the current CVE
+
+                # Iterate over unique ICSAs associated with this CVE
+                for icsa in unique_icsas:
+                    # Insert each CVE and ICSA into the cve_icsa_join table
+                    cur.execute("INSERT INTO cve_icsa_join (cve_id, icsa) VALUES (%s, %s) ON CONFLICT DO NOTHING", (cve, icsa))
+
         conn.commit()
 
     print(f"Entries inserted or skipped: {len(entries)}")
